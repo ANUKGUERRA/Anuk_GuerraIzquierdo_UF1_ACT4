a23c4622413ee1df91675308e408bdba03367564
ANUKGUERRA <anukguerra@gmail.com>
2024-10-21 12:22:40 +0200

    Finalitzat

--- app/src/main/java/com/example/anuk_guerraizquierdo_uf1_act4/MainActivity.kt
+++ app/src/main/java/com/example/anuk_guerraizquierdo_uf1_act4/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.anuk_guerraizquierdo_uf1_act4
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val logCat = findViewById<Button>(R.id.LogCat)
+        logCat.setOnClickListener{
+            Log.d("LogCat:", "S'ha clicat el boto (logCat)")
+        }
+
+        val toast = findViewById<Button>(R.id.Toast)
+        toast.setOnClickListener{
+            Toast.makeText(this, "S'ha clicat el boto (toast)", Toast.LENGTH_SHORT).show();
+        }
     }
+
 }
